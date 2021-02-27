7c57e9d2899d4632fa314a19aa8fe80113ea12de
manufacen <efacendini@gmail.com>
2021-02-26 22:37:41 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import CountryList from "./components/CountryList";
+import CountryList from './components/CountryList'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './components/reducer'
+import ActionList from './components/actionList'
+import Header from './components/header'
+import CountryPage from './components/countryPage'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom'
+
+const initialState = {
+  countryList: [],
+  countryListByName: [],
+  coutryFilteredByRegion: [],
+  filterByRegion: '',
+
+}
+
+const store = createStore(reducer, initialState)
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
+  const [checked, setChecked] = useState(false)
+  const mainClass = darkMode ? 'is-dark-mode' : 'is-light-mode'
+
+  function changeMedia(mq) {
+    setDarkMode(mq.matches)
+    setChecked(mq.matches)
+  }
+
+  useEffect(() => {
+    const mq = window.matchMedia('(prefers-color-scheme: dark)')
+    mq.addListener(changeMedia)
+    setDarkMode(mq.matches)
+    setChecked(mq.matches)
+    return () => {
+      mq.removeListener(changeMedia)
+    }
+  }, [])
   return (
-    <div className="App">
-      <CountryList />    
-    </div>
+    <main className={mainClass}>
+      <Provider store={store}>
+        <Router>
+          <Header setDarkMode={setDarkMode} darkMode={darkMode} />
+          <Switch>
+            <Route path="/country/:id" component={CountryPage} />
+            <Route path="/">
+              <ActionList />
+              <CountryList />
+            </Route>
+          </Switch>
+        </Router>
+      </Provider>
+    </main>
   );
 }
 
